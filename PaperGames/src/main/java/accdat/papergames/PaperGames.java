9ae1d10ba67879f12cd300e4af5ee67b501091f8
Lucas Serrano Pérez <lmserranop03@educastillalamancha.es>
2025-01-08 13:38:03 +0100

    creation of project

---
+++ PaperGames/src/main/java/accdat/papergames/PaperGames.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package accdat.papergames;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PaperGames {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
