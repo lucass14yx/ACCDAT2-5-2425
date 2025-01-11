4846804bddae0824c150b13fec5e4a4c173eeaca
Lucas Serrano Pérez <lmserranop03@educastillalamancha.es>
2025-01-11 15:57:57 +0100

    botones y funcionalidad de prueba

--- PaperGames/src/main/java/accdat/papergames/Controlador/Controlador.java
+++ PaperGames/src/main/java/accdat/papergames/Controlador/Controlador.java
@@ -4,26 +4,39 @@
  */
 package accdat.papergames.Controlador;
 
+import accdat.papergames.Modelo.Modelo;
 import accdat.papergames.Vista.InterfazVista;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 
 /**
  *
  * @author USUARIO
  */
-public class Controlador{
+public class Controlador implements ActionListener{
     private final InterfazVista vista;
+    private final Modelo modelo;
     
 
-    public Controlador(InterfazVista vista) {
+    public Controlador(InterfazVista vista, Modelo modelo) {
         this.vista = vista;
+        this.modelo = modelo;
        
         
         this.vista.setControlador(this);
         this.vista.arranca();
     }
     
-    
-    
+
+    @Override
+    public void actionPerformed(ActionEvent evento) {
+        switch (evento.getActionCommand()){
+            case InterfazVista.PRUEBA1 -> vista.haceAlgo("has pulsado el primer boton");
+            case InterfazVista.PRUEBA2 -> vista.haceAlgo("has pulsado el segundo boton");
+        }
+
+    }
    
 }
