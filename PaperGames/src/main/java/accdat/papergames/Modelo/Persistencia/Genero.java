1d0e182cf2090a0738704b8b47f38b4fcece8478
rezzt.dev <rezzt.dev@proton.me>
2025-01-11 13:39:54 +0100

    update | creacion de clases y controladores de persistencia

---
+++ PaperGames/src/main/java/accdat/papergames/Modelo/Persistencia/Genero.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package accdat.papergames.Modelo.Persistencia;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Collection;
+
+/**
+ *
+ * @author rezzt
+ */
+@Entity
+@Table(name = "GENERO")
+@NamedQueries({
+  @NamedQuery(name = "Genero.findAll", query = "SELECT g FROM Genero g"),
+  @NamedQuery(name = "Genero.findByNombreGenero", query = "SELECT g FROM Genero g WHERE g.nombreGenero = :nombreGenero")})
+public class Genero implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  @Id
+  @Basic(optional = false)
+  @NotNull
+  @Size(min = 1, max = 100)
+  @Column(name = "NOMBRE_GENERO")
+  private String nombreGenero;
+  @OneToMany(mappedBy = "nombreGenero")
+  private Collection<Videojuego> videojuegoCollection;
+
+  public Genero() {
+  }
+
+  public Genero(String nombreGenero) {
+    this.nombreGenero = nombreGenero;
+  }
+
+  public String getNombreGenero() {
+    return nombreGenero;
+  }
+
+  public void setNombreGenero(String nombreGenero) {
+    this.nombreGenero = nombreGenero;
+  }
+
+  public Collection<Videojuego> getVideojuegoCollection() {
+    return videojuegoCollection;
+  }
+
+  public void setVideojuegoCollection(Collection<Videojuego> videojuegoCollection) {
+    this.videojuegoCollection = videojuegoCollection;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash += (nombreGenero != null ? nombreGenero.hashCode() : 0);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    // TODO: Warning - this method won't work in the case the id fields are not set
+    if (!(object instanceof Genero)) {
+      return false;
+    }
+    Genero other = (Genero) object;
+    if ((this.nombreGenero == null && other.nombreGenero != null) || (this.nombreGenero != null && !this.nombreGenero.equals(other.nombreGenero))) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "accdat.papergames.Modelo.Persistencia.Genero[ nombreGenero=" + nombreGenero + " ]";
+  }
+  
+}
---
+++ PaperGames/src/main/java/accdat/papergames/Modelo/Persistencia/ModoJuego.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package accdat.papergames.Modelo.Persistencia;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Collection;
+
+/**
+ *
+ * @author rezzt
+ */
+@Entity
+@Table(name = "MODO_JUEGO")
+@NamedQueries({
+  @NamedQuery(name = "ModoJuego.findAll", query = "SELECT m FROM ModoJuego m"),
+  @NamedQuery(name = "ModoJuego.findByNombreModoJuego", query = "SELECT m FROM ModoJuego m WHERE m.nombreModoJuego = :nombreModoJuego")})
+public class ModoJuego implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+  @Id
+  @Basic(optional = false)
+  @NotNull
+  @Size(min = 1, max = 100)
+  @Column(name = "NOMBRE_MODO_JUEGO")
+  private String nombreModoJuego;
+  @ManyToMany(mappedBy = "modoJuegoCollection")
+  private Collection<Videojuego> videojuegoCollection;
+
+  public ModoJuego() {
+  }
+
+  public ModoJuego(String nombreModoJuego) {
+    this.nombreModoJuego = nombreModoJuego;
+  }
+
+  public String getNombreModoJuego() {
+    return nombreModoJuego;
+  }
+
+  public void setNombreModoJuego(String nombreModoJuego) {
+    this.nombreModoJuego = nombreModoJuego;
+  }
+
+  public Collection<Videojuego> getVideojuegoCollection() {
+    return videojuegoCollection;
+  }
+
+  public void setVideojuegoCollection(Collection<Videojuego> videojuegoCollection) {
+    this.videojuegoCollection = videojuegoCollection;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash += (nombreModoJuego != null ? nombreModoJuego.hashCode() : 0);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    // TODO: Warning - this method won't work in the case the id fields are not set
+    if (!(object instanceof ModoJuego)) {
+      return false;
+    }
+    ModoJuego other = (ModoJuego) object;
+    if ((this.nombreModoJuego == null && other.nombreModoJuego != null) || (this.nombreModoJuego != null && !this.nombreModoJuego.equals(other.nombreModoJuego))) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "accdat.papergames.Modelo.Persistencia.ModoJuego[ nombreModoJuego=" + nombreModoJuego + " ]";
+  }
+  
+}
